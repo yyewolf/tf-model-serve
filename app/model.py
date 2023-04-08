4c8e59d24ab9a240deea832a880add5640e0a3d8
Yewolf <yyewolf@gmail.com>
2023-04-08 14:28:21 +0200

    feat: added h5 support

--- app/model.py
+++ app/model.py
@@ -9,14 +9,34 @@ class Model():
         self.path = f"models/{name}/"
         self.labels = json.load(open(self.path+'labels.json', 'r'))
         self.params = json.load(open(self.path+'params.json', 'r'))
-        self.loaded_model = tf.saved_model.load(self.path)
-        self.predict_fn = self.loaded_model.signatures['serving_default']
-
         self.IMG_HEIGHT = self.params.get('IMG_HEIGHT', 224)
         self.IMG_WIDTH = self.params.get('IMG_WIDTH', 224)
         self.OUTPUT = self.params.get('OUTPUT', 3)
-
+        self.LEGACY = self.params.get('LEGACY', True)
+        if self.LEGACY:
+            self.loaded_model = tf.saved_model.load(self.path)
+            self.predict_fn = self.loaded_model.signatures['serving_default']
+        else:
+            self.loaded_model = tf.keras.models.load_model(self.path+"model.h5")
+            
     def predict(self, img, k: int):
+        if self.LEGACY:
+            return self.predict_legacy(img, k)
+        return self.predict_h5(img, k)
+    
+    def predict_h5(self, img, k: int):
+        # Call the prediction function to get the model's prediction for your test image
+        predictions = self.loaded_model.predict(img)
+        
+        # Extract the predicted class probabilities from the output dictionary
+        predicted_probabilities = predictions[0]
+        
+        # Get the top 3 predicted class indices and their corresponding probabilities
+        top_k_indices = np.argsort(predicted_probabilities)[::-1][:k]
+        top_k_probabilities = predicted_probabilities[top_k_indices] * 100
+        return [(self.labels[str(top_k_indices[i])], top_k_probabilities[i]) for i in range(len(top_k_indices))]
+        
+    def predict_legacy(self, img, k: int):
         # Call the prediction function to get the model's prediction for your test image
         predictions = self.predict_fn(tf.constant(img))
 
@@ -28,3 +48,4 @@ class Model():
         top_k_indices = np.argsort(predicted_probabilities)[::-1][:top_k]
         top_k_probabilities = predicted_probabilities[top_k_indices] * 100
         return [(self.labels[str(top_k_indices[i])], top_k_probabilities[i]) for i in range(len(top_k_indices))]
+
